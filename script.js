37cf080803a37fba88906915f3d782da98211181
AthrvTech <mrhackershivansh143@gmail.com>
2025-06-09 05:00:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 function toggleMenu() {
-  const nav = document.getElementById('navMenu');
-  nav.classList.toggle('active');
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('active');
 }
 
 // Typing animation
-const text = "Quantum is Progressing 🚀";
-let index = 0;
-const speed = 100;
+const phrases = ["Quantum Ideas.", "AI Innovations.", "Your Digital Future."];
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
 
-function typeEffect() {
-  if (index < text.length) {
-    document.getElementById("typingText").innerHTML += text.charAt(index);
-    index++;
-    setTimeout(typeEffect, speed);
+function loop() {
+  isEnd = false;
+  document.getElementById("typing-text").innerHTML = currentPhrase.join('');
+
+  if (i < phrases.length) {
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j]);
+      j++;
+      document.getElementById("typing-text").innerHTML = currentPhrase.join('');
+    }
+
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j]);
+      j--;
+      document.getElementById("typing-text").innerHTML = currentPhrase.join('');
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = [];
+      isDeleting = false;
+      i++;
+      if (i === phrases.length) {
+        i = 0;
+      }
+    }
   }
+
+  const speed = isEnd ? 2000 : isDeleting ? 50 : 150;
+  setTimeout(loop, speed);
 }
 
-window.onload = typeEffect;
+loop();
